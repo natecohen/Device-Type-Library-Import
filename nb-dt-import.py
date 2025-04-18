e0df1c27102de589f28d7105fe4a0f2e70585dd1
Bastian Leicht <b.leicht@bastianleicht.de>
2025-04-18 15:33:36 +0200

    feat: add functionality for importing rack-types

--- nb-dt-import.py
+++ nb-dt-import.py
@@ -27,6 +27,15 @@ def main():
         netbox.create_manufacturers(vendors)
         netbox.create_module_types(module_types)
 
+    if netbox.rack_types:
+        settings.handle.log("Rack-Types Enabled. Creating Racks...")
+        files, vendors = settings.dtl_repo.get_devices(f"{settings.dtl_repo.repo_path}/rack-types/", args.vendors)
+        settings.handle.log(f"{len(vendors)} Rack Vendors Found")
+        rack_types = settings.dtl_repo.parse_files(files, slugs=args.slugs)
+        settings.handle.log(f"{len(rack_types)} Rack-Types Found")
+        netbox.create_manufacturers(vendors)
+        netbox.create_rack_types(rack_types)
+
     settings.handle.log("---")
     settings.handle.verbose_log(f"Script took {time.perf_counter() - start_time:.2f} seconds to run")
     settings.handle.log(f"{netbox.counter['added']} devices created")
@@ -36,6 +45,8 @@ def main():
     if netbox.modules:
         settings.handle.log(f"{netbox.counter['module_added']} modules created")
         settings.handle.log(f"{netbox.counter['module_port_added']} module interface / ports created")
+    if netbox.rack_types:
+        settings.handle.log(f"{netbox.counter['rack_types_added']} rack-types created")
 
 
 if __name__ == "__main__":
